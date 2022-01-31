66b0c0864cdb7696229e6b798d2b1d90cec90471
Dennis Savai <dennis.savai316@gmail.com>
2022-01-31 12:23:16 +0300

    added files

--- js/build.js
+++ js/build.js
@@ -1,4 +1,7 @@
 const header = document.querySelector(".header")
+const mobileMenu = document.querySelector("#mobile-menu")
+const body = document.querySelector("body")
+const scrollLinks = document.querySelectorAll(".navigation-list li a")
 
 function scrollHeader() {
     const headerHeight = header.getBoundingClientRect().height
@@ -12,8 +15,36 @@ function scrollHeader() {
             header.classList.add("scrolled-header")
         }
     })
-
-
 }
 
 scrollHeader()
+
+mobileMenu.addEventListener("click", function (event) {
+    body.classList.toggle("menu-open")
+})
+
+//navigation
+if (scrollLinks) {
+    scrollLinks.forEach((link) => {
+		link.addEventListener("click", function (event) {
+			event.preventDefault()
+			const id = event.currentTarget.getAttribute("href").slice(1)
+			const element = document.getElementById(id);
+			
+			const headerHeight = header.getBoundingClientRect().height;
+            console.log("hi")
+			
+			let position = element.offsetTop - headerHeight;
+
+			window.scrollTo({
+				left: 0,
+                top: position,
+                behavior: 'smooth'
+            });
+
+            body.classList.remove("menu-open")
+        })
+     
+	})
+}
+
